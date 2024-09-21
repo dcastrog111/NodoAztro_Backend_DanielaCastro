cdbf6a1c91318e3653ba5904b858817d2dcbeb72
Daniela Castro <dcastrog1@eafit.edu.co>
2024-09-20 22:47:08 -0500

    Creacion de clases

---
+++ Ejercicio3_Agencia_Viajes/src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.domain.Administrador;
+import org.example.domain.Cliente;
+import org.example.domain.Persona;
+
+import java.util.ArrayList;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Administrador p = new Administrador();
+        p.setId("11111");
+        System.out.println(p.getId());
+        p.setDepartamento("Zona Norte");
+        System.out.println(p.getDepartamento());
+
+        Cliente c1 = new Cliente();
+
+
+
+
+
+    }
+}
